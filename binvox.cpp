cfbd3a27d6537e2f324c6fd07a80dd849b33b48e
Sébastien Loriot <sebastien.loriot@cgal.org>
2020-01-01 12:04:58 +0100

    write the file and use middle of voxel as query

--- binvox.cpp
+++ binvox.cpp
@@ -42,14 +42,16 @@ int main(int argc, char** argv)
 
   // TODO: extract from command line
   const std::size_t grid_size = 10;
+  double gs_d = grid_size;
   CGAL::Bbox_3 bbox = PMP::bbox(mesh);
   // identify the longest side
   double extend = bbox.xmax() - bbox.xmin();
   extend = (std::max)(extend, bbox.ymax() - bbox.ymin());
   extend = (std::max)(extend, bbox.zmax() - bbox.zmin());
   extend/=grid_size;
+  Point_3 origin(bbox.xmin(), bbox.ymin(), bbox.zmin());
 
-  std::ofstream out("output.bin"); // TODO: extract name from commandline
+  std::ofstream out("output.bin", std::ios_base::binary); // TODO: extract name from commandline
   out << "#binvox 1\n";
   out << "dim " << grid_size << " " << grid_size << " " << grid_size << "\n";
   out << "translate " << -origin.x() << " " << -origin.y() << " " << -origin.z() << "\n";
@@ -57,13 +59,31 @@ int main(int argc, char** argv)
   out << "data\n";
 
   CGAL::Side_of_triangle_mesh<Mesh, K> side_of(mesh);
-  Point_3 origin(bbox.xmin(), bbox.ymin(), bbox.zmin());
-  for (std::size_t i=0; i<=grid_size; ++i)
-    for(std::size_t j=0; j<=grid_size; ++j)
-      for(std::size_t k=0; k<=grid_size; ++k)
+  
+
+  typedef unsigned char byte;
+  bool prev=false;
+  byte count=0;
+  for (double xi=0; xi<gs_d; ++xi)
+    for(double zi=0; zi<gs_d; ++zi)
+      for(double yi=0; yi<gs_d; ++yi)
       {
-        Point_3 query = origin + K::Vector_3(i*extend, j*extend, k*extend);
+        Point_3 query = origin + K::Vector_3((xi+0.5)*extend, (yi+0.5)*extend, (zi+0.5)*extend);
         bool inside = query.x()<=bbox.xmax() && query.y()<=bbox.ymax() && query.z()<=bbox.zmax()
                       && side_of(query) != CGAL::ON_UNBOUNDED_SIDE;
+        if (xi==0 && yi==0 && zi==0)
+          prev = inside;
+        if (prev==inside)
+          ++count;
+        else
+        {
+          byte value=prev;
+          out << value << count;
+          prev=inside;
+        }
       }
+  byte value=prev;
+  out << value << count;
+
+  return 0;
 }
